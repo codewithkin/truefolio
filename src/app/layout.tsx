e38e9a637e8f529ad5bd71766af9ff945e1cac03
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-07-12 12:44:39 +0200

    Refactor Landing component and remove unused code for cleaner structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import DesktopNavbar from "@/components/shared/landing/DesktopNavbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <DesktopNavbar/>
         {children}
       </body>
     </html>
